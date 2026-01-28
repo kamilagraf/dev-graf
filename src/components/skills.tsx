7e349d0cf27d675a4d3a7b3ffe6d811ecbf56799
Kamila Graf <kam.graf0@gmail.com>
2026-01-28 17:35:12 +0100

    Add skills section placeholder

---
+++ src/components/skills.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export function Skills() {
+  return (
+    <div className="max-w-7xl mx-auto px-8 py-20">
+      <h2 className="text-5xl font-bold dark:text-white text-zinc-700 mb-4">Tech stack</h2>
+      <div className="h-1 w-20 bg-purple-600"></div>
+    </div>
+  );
+}
+
